36050010c549426bdc9faf21f604c5ae8864f58b
Filipe Santos <filipesantos1999@hotmail.com>
2023-03-06 19:32:40 +0000

    Add integration tests for Nim

---
+++ nim/tests/integration/integration.nim
@@ -0,0 +1,42 @@
+# internal
+import dependency/fibonacci
+import dependency/square
+import dependency/constructor
+import dependency/setter
+import dependency/interface_injection
+
+# external
+import unittest2
+
+
+test "integration__constructor__do_all__valid_data":
+    let fibonacci = new_fibonacci()
+    let square = new_square()
+    let constructor = new_constructor(fibonacci, square)
+
+    constructor.do_all(10'u32)
+
+    check true == true
+
+test "integration__setter__do_all__valid_data":
+    let fibonacci = new_fibonacci()
+    let square = new_square()
+    let setter = new_setter()
+    setter.set_fibonacci(fibonacci)
+    setter.set_square(square)
+
+    setter.do_all(10'u32)
+
+    check true == true
+
+test "integration__interface_injection__do_all__valid_data":
+    let fibonacci = new_fibonacci()
+    let interface_injection = new_interface_injection()
+
+    interface_injection.compute_service(
+        "Fibonacci",
+        fibonacci,
+        10
+    )
+
+    check true == true
